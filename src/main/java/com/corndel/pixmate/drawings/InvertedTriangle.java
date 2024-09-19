fcdd6a3799d726d7ba3a4ad395e13241325c1418
Human0467 <mustchjo@gmail.com>
2024-09-19 14:24:46 +0100

    invertedTriangle() complete

--- src/main/java/com/corndel/pixmate/drawings/InvertedTriangle.java
+++ src/main/java/com/corndel/pixmate/drawings/InvertedTriangle.java
@@ -1,5 +1,7 @@
 package com.corndel.pixmate.drawings;
 
+import com.github.tomaslanger.chalk.Chalk;
+
 public class InvertedTriangle {
 
     public static void main(String[] args) {
@@ -9,9 +11,15 @@ public class InvertedTriangle {
 
     public static void invertedTriangle(int rows){
 
+        StringBuilder line = new StringBuilder();
+        // loop over rows
         for (int i = 0; i < rows; i++) {
-
-
+            // draw correct number of stairs
+            for(int j = 0; j < (rows-i); j++){
+                line.append(Chalk.on("*").green());
+            }
+            line.append("\n");
         }
+        System.out.println(line.toString());
     }
 }

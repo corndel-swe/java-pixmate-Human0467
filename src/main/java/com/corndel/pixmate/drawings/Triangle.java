0e51009d9161ae8a71b098f3016b0dd4d660b456
Human0467 <mustchjo@gmail.com>
2024-09-19 14:14:21 +0100

    drawTriangle() method

--- src/main/java/com/corndel/pixmate/drawings/Triangle.java
+++ src/main/java/com/corndel/pixmate/drawings/Triangle.java
@@ -1,4 +1,25 @@
 package com.corndel.pixmate.drawings;
 
+import com.github.tomaslanger.chalk.Chalk;
+
 public class Triangle {
+
+    public static void main(String[] args) {
+
+        drawTriangle(5);
+    }
+
+    public static void drawTriangle(int rows){
+
+        // loop over rows
+        for(int i = 0; i < rows; i++){
+            StringBuilder line = new StringBuilder();
+            for(int j = 0; j <= i; j++){
+                // add required numbers of *s to line
+                line.append(Chalk.on("*").green());
+            }
+            // print this row
+            System.out.println(line.toString());
+        }
+    }
 }
